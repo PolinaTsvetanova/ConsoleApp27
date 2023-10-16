0fb932719f2f99dcf0c76be97e140d4073184b15
RadoslavTsvetkov <negrebyt@gmail.com>
2023-10-16 15:56:43 +0300

    нещо

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp27
     {
         static void Main(string[] args)
         {
-            
-        }
+            Console.WriteLine("Въведете числото n");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i += 1)
+            {
+                Console.WriteLine(new string('*', n));
+            }
+
+        }    
     }
 }
